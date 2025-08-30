9d5f2baaac9a7b475aac83d444b3f907639457c9
Oyeks_Ayo <adamsonia62.gmail.com>
2025-08-30 19:05:38 +0100

    cleanup and config updates

--- pkg/config.py
+++ pkg/config.py
@@ -2,5 +2,5 @@ import os
 
 class Appconfig(object):
     SECRET_KEY = os.getenv("SECRET_KEY", 'fallback_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
