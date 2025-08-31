cd0d20186c40b6ae67518abedcc95ef7a860ed9e
Oyeks_Ayo <adamsonia62.gmail.com>
2025-08-31 16:28:29 +0100

    port no

--- starter.py
+++ starter.py
@@ -1,4 +1,8 @@
+import os
 from pkg import app
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+    # app.run(debug=False, port=5000)
+
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
